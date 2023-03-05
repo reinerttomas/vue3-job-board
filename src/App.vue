d3419f713d285c74b88d801b3d2e751add0f4488
Tomáš Reinert <reinerttomas@gmail.com>
2023-03-05 14:20:57 +0100

    init project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
